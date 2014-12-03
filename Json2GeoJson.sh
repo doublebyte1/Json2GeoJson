2dcd28ecd62978398cc6496bbcf26a3f789046ee
doublebyte <jo@doublebyte.net>
2014-12-03 15:47:40 +0100

    - updated to the correct script

--- Json2GeoJson.sh
+++ Json2GeoJson.sh
@@ -1,16 +1,18 @@
 
-#remove olr
-#sed -i 's/olr://g' $1
 
 #Open GeoJSON
 echo  "{ \"type\": \"FeatureCollection\", \"features\": [" > $2
 
-#query reportSegments
- cat $1 | jq -c '.Inrix.Dictionary.Report.reportSegments[] | .ReportSegmentLRC.method.locationReference.optionLinearLocationReference.first.coordinate.latitude as $lat1 | .ReportSegmentLRC.method.locationReference.optionLinearLocationReference.first.coordinate.longitude as $lon1 | .ReportSegmentLRC.method.locationReference.optionLinearLocationReference.last.coordinate.longitude as $lon2 | .ReportSegmentLRC.method.locationReference.optionLinearLocationReference.last.coordinate.latitude as $lat2 | def signO(f): if f> 0 then 1 elif f==0 then 0 else -1 end;   {  "properties": { "Name": .SegmentNames.Name, "ReportSegmentID": .ReportSegmentID }, "geometry": { "type": "LineString", "coordinates": [ [(($lon1-(signO($lon1)*0.5))*360)/16777216, (($lat1-(signO($lat1)*0.5))*360)/16777216], [((($lon1-(signO($lon1)*0.5))*360)/16777216) + $lon2/100000, ((($lat1-(signO($lat1)*0.5))*360)/16777216) + $lat2/100000]]}, "type": "Feature"} ' >> $2
-
-
+#query tramos 
+cat $1 | jq  -c '.tramos[]| {"type": "Feature","geometry": {"type": "LineString", "coordinates": .points | map([.lon,.lat])},"properties":{"name": .name, "id": .id}}' >> $2
+ 
 #close GeoJSON 
 echo "]}" >> $2
 
+#remove line breaks
+sed -i ':a;N;$!ba;s/\n/\t/g' $2
+
 #add comas
-sed -i 's/}}/}},/g' $2
+sed -i 's/}\t{/},{/g' $2 
+ 
+ 
